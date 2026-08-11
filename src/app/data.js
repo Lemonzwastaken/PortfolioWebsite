9b10d00c957066eae0218c588303073207ecffbe
Aaradhy Srivastava <aaradhyarya.srivastava@gmail.com>
2026-08-11 12:02:49 +0530

    Added navigation buttons and small changes to files and stuff

--- src/app/data.js
+++ src/app/data.js
@@ -1,11 +1,4 @@
 /*
-Websites:
-
-- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
-- https://lucide.dev/icons/ (Lucide Icons)
-- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
-- https://skillicons.dev (Skill Icons to show skills)
-- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
 
 :root {
   --background: 27 27 27;
@@ -96,19 +89,19 @@ export const BtnList = [
   { label: "Contact", link: "/contact", icon: "contact", newTab: false },
   {
     label: "Github",
-    link: "https://www.github.com/codebucks27",
+    link: "https://github.com/lemonzwastaken",
     icon: "github",
     newTab: true,
   },
   {
     label: "LinkedIn",
-    link: "https://www.linkedin.com/in/codebucks",
+    link: "https://www.linkedin.com/in/aaradhy-srivastava-6a93b6371/",
     icon: "linkedin",
     newTab: true,
   },
   {
-    label: "X",
-    link: "https://www.x.com/code_bucks",
+    label: "Music",
+    link: "https://open.spotify.com/artist/3xrRSYAIhqv4ijzPDXkg0c",
     icon: "twitter",
     newTab: true,
   },
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import bg from "../../public/background/home-background.png";
 import RenderModel from "@/components/RenderModel";
 import Atm from "@/components/models/Atm";
+import Navigation from "@/components/Navigation";
 
 export default function Home() {
   return (
@@ -9,11 +10,15 @@ export default function Home() {
       <Image src={bg} alt="background-image" fill className="object-cover object-center opacity-25" />
 
       <div className="w-full h-screen">
+        <Navigation />
         {<RenderModel>
           <Atm />   
+          console.log();
+          
         </RenderModel>}
+        
       </div>
-
+    
 
     </main>
   );
