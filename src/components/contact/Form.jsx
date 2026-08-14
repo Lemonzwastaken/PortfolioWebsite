a4ad12d628977ed84f557628dc64d6e9a0801608
Aaradhy Srivastava <aaradhyarya.srivastava@gmail.com>
2026-08-14 15:29:10 +0530

    Linked email.js to the contact me page

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -2,10 +2,46 @@
 
 import React from 'react';
 import { useForm } from "react-hook-form";
+import emailjs from "@emailjs/browser"
+
 
 export default function Form() {
   const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+
+  const sendEmail = (templateParams) => {
+    emailjs
+        .send(
+          process.env.NEXT_PUBLIC_SERVICE_ID,
+          process.env.NEXT_PUBLIC_TEMPLATE_ID,
+          templateParams,
+          {
+            publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+            limitRate: {
+              throttle: 5000, // cannot send more than one email in 5 seconds
+            },
+          }
+        )
+        .then(
+            () => {
+                console.log("Successful")
+            },
+            (error) => {
+                console.log('Failed....', error.text)
+            }
+        );
+  };
+
+  const onSubmit = (data) => {
+
+    const templateParams = {
+        to_name: "Aaradhy",
+        name: data.name,
+        email: data.email,
+        message: data.message,
+    }
+
+    sendEmail(templateParams)
+  }
   console.log(errors);
 
   return (
@@ -21,14 +57,21 @@ export default function Form() {
       <input
         type="email"
         placeholder="Email"
-        {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
+        {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
         className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'
       />
-      <textarea
-        placeholder="Message"
-        {...register("message", { required: true, maxLength: 256, minLength: 50 })}
-        className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'
-      />
+      <div className='w-full'>
+        <textarea
+          placeholder="Message"
+          {...register("message", { required: true, maxLength: 256, minLength: 50 })}
+          className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'
+        />
+        {errors.message && (
+          <p className='text-red-400 text-sm mt-1'>
+            Message must be greater than 50 characters
+          </p>
+        )}
+      </div>
       <input
       value="Send your message"
       className='px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 
