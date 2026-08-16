49c9846a0d02876fb62127909cdbdb0e3ce83130
Aaradhy Srivastava <aaradhyarya.srivastava@gmail.com>
2026-08-16 18:18:11 +0530

    Small tweaks

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/PortfolioWebsite',
+  assetPrefix: '/PortfolioWebsite',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
